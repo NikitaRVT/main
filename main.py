455f9d6bdcba8a1436622392715d6e58d7de72be
Nikita Ivanov <nikitaivanov135@gmail.com>
2020-01-14 10:01:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/index")
+@app.route('/home')
+def home():
+  return render_template('home.html')
+
+@app.route("/about")
+def getAbout():
+  return render_template('about.html')
+
+@app.route("/contact")
+def contact():
+  return render_template('contact.html', phone = 87654321)
+
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", threaded=True, port=5000, debug=True)
