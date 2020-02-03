764cfed7870022aa95c2f542ace4710db3bf7ff8
[Students] Nikita Ivanov <81DNIvanov@rvt.lv>
2020-02-03 15:32:41 +0200

    GET, POST

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+from fl1 import read_file, write_file
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
-@app.route('/home')
+@app.route("/home")
 def home():
   return render_template('home.html')
 
@@ -19,6 +19,33 @@ def contact():
 @app.route("/params")
 def params():
   return request.args
+
+@app.route("/post_req" , methods=['POST'])
+def post_req():
+  return request.args
+
+@app.route("/read_file")
+def read_from_file():
+  content = read_file()
+  return content
+
+@app.route("/write_file", methods=['POST'])
+def write_to_file():
+  if request.content_type == 'aplication/json':
+    contentJSON = request.get_json()
+    write_file(contentJSON['data'])
+    return f"Add line{contentJSON['data']} to file"
+  else:
+    return f"Invalid request{request.content_type} not allowed"
+
+@app.route('/file', methods = ['GET', 'POST'])
+def filework():
+  if request.method == 'GET':
+    return read_from_file()
+  elif request.method == 'POST':
+    return write_to_file()
+  else:
+    return f"Invalid request{request.method} not allowed"
 
 
 if __name__ == '__main__':
